493201aa90c83b798d2c8aa174e27bd0108ff931
SANG-YOUP-LEE <87376461+SANG-YOUP-LEE@users.noreply.github.com>
2025-07-08 11:09:18 +0900

    테스트

--- src/components/AddRoutineSelector.vue
+++ src/components/AddRoutineSelector.vue
@@ -7,7 +7,7 @@
 
 			<!-- 다짐명 적기 -->
 			<div class="form_box_g rt_make">
-				<h3>자, 다짐명 부터 적어볼까요?</h3>
+				<h3>다짐명을 적어주세요.</h3>
 				<p>
 					<label for="rt_name">새 다짐 명</label>
 					<input type="text" placeholder="ex)외로워도 슬퍼도 탄수화물 끊기">
@@ -20,17 +20,32 @@
 			<div class="form_box_g rt_make">
 				<h3>얼마나 자주 지켜야 하나요?</h3>
 				<p>
-					<button class="btn_basic">나만의 반복 주기 만들기</button>
+					<button>나만의 반복 주기 만들기</button>
 				</p>
-				<p>
-					<label class="toggle-switch">
-						<input type="checkbox" />
-						<span class="slider"></span>
-					</label>
-					반복 없이 하루만
-				</p>
+				<div class="rt_make_dtail>
+				반복주기 펼침
+				</div>
 			</div>
 			<!--// 다짐 주기 설정 -->
+			
+			<!-- 다짐 시작일 설정 -->
+			<div class="form_box_g rt_make">
+				<h3>언제부터 시작할건가요?</h3>
+				<p>
+					
+				</p>
+			</div>
+			<!--// 다짐 시작일 설정 -->
+			
+			
+			<!-- 다짐 알람 설정 -->
+			<div class="form_box_g rt_make">
+				<h3>알람이 필요한가요?</h3>
+				<p>
+					
+				</p>
+			</div>
+			<!--// 다짐 알람 설정 -->
 			
 			<!-- 러피 산책 설정 -->
 			<div class="form_box_g rt_make">
@@ -57,7 +72,7 @@
 			<div class="form_box_g rt_make">
 				<h3>소곤소곤 더 할 말은 없나요?</h3>
 				<p>
-					<textarea row="2">다짐 저장하기 전 마지막 할말은 없나요? 아자아자</textarea>
+					<textarea row="2">좀더 하고 싶은 말은 없나요?</textarea>
 				</p>
 			</div>
 			<!--// 메세지 설정 -->			
