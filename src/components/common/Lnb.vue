49fd090155e520449b170189817aff9fa70c2807
SANG-YOUP-LEE <87376461+SANG-YOUP-LEE@users.noreply.github.com>
2025-07-09 09:25:15 +0900

    Lnb.vue 업데이트

--- src/components/common/Lnb.vue
+++ src/components/common/Lnb.vue
@@ -2,8 +2,9 @@
     <div class="popup_overlay">
         <!-- lnb -->
         <div class="lnb_wrap">
-            LNB Area
-
+            <p><span>러피님</span> 환영합니다</p>
+						<button>로그아웃</button>
+						<button>회원탈퇴</button>
             <button class="close_btn"  @click.prevent="$emit('close')"><span>팝업 닫기</span></button>
         </div>
         <!--// lnb -->
