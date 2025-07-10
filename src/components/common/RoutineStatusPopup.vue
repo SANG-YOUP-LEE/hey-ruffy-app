3b511d0024f451131a4df13ed97c1721ed19740c
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-10 13:38:12 +0900

    RoutineStatusPopup.vue 만들기

---
+++ src/components/common/RoutineStatusPopup.vue
@@ -0,0 +1,14 @@
+<template>
+	<!-- 레이어 팝업 -->
+	<div class="popup_overlay>
+    <!-- 오늘의 다짐 달성 현황 -->
+    <div class="popup_box">
+     <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
+     <div class="done_check_pop">
+      <h2>Step2 달성 완료!</h2>
+				아하함! 슬슬 산책 준비해볼까멍!
+     </div>
+    </div>
+   	<!-- // 오늘의 다짐 달성 현황 -->
+	</div>
+</template>
