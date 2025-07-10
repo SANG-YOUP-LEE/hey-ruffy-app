f88598bdbbcd124a1e9fdac131d52da5926bd5c5
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-10 13:45:05 +0900

    RoutineStatusPopup.vue 업데이트

--- src/components/common/RoutineStatusPopup.vue
+++ src/components/common/RoutineStatusPopup.vue
@@ -1,14 +1,54 @@
 <template>
 	<!-- 레이어 팝업 -->
-	<div class="popup_overlay>
-    <!-- 오늘의 다짐 달성 현황 -->
-    <div class="popup_box">
+	<div class="popup_overlay">
+    <!-- 산책가기 1단계 -->
+    <div class="popup_box w_step01">
      <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
      <div class="done_check_pop">
       <h2>Step2 달성 완료!</h2>
-				아하함! 슬슬 산책 준비해볼까멍!
+				아직 산책가려면 멀었나멍~
      </div>
     </div>
-   	<!-- // 오늘의 다짐 달성 현황 -->
+   	<!-- // 산책가기 1단계 -->
+
+		<!-- 산책가기 2단계 -->
+    <div class="popup_box w_step02">
+     <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
+     <div class="done_check_pop">
+      <h2>Step2 달성 완료!</h2>
+				이제 슬슬 산책갈 준비를 해볼까멍~
+     </div>
+    </div>
+   	<!-- // 산책가기 2단계 -->
+
+		<!-- 산책가기 3단계 -->
+    <div class="popup_box w_step03">
+     <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
+     <div class="done_check_pop">
+      <h2>Step3 달성 완료!</h2>
+				빨리 빨리 줄챙기라멍! 심장이 콩닥콩닥 꼬리가 휭휭휭휭! 
+     </div>
+    </div>
+   	<!-- // 산책가기 3단계 -->
+
+		<!-- 산책가기 4단계 -->
+    <div class="popup_box w_step04">
+     <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
+     <div class="done_check_pop">
+      <h2>Step4 달성 완료!</h2>
+				꺄하하하! 날아갈것 같다멍!
+     </div>
+    </div>
+   	<!-- // 산책가기 4단계 -->
+
+		<!-- 산책가기 5단계 -->
+    <div class="popup_box w_step04">
+     <button class="close_btn" @click="closePopup"><span>팝업 닫기</span></button>
+     <div class="done_check_pop">
+      <h2>Step5 달성 완료!</h2>
+				간질간질 산책 후 목욕은 필수라멍!
+     </div>
+    </div>
+   	<!-- // 산책가기 5단계 -->
 	</div>
 </template>
