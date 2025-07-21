0dfe6771f7962764aeda0127a92bc7272c6098a5
SANG-YOUP-LEE <dak7@naver.com>
2025-07-21 20:10:04 +0900

    러피 선택

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -73,19 +73,22 @@
                     <img src="/src/assets/images/ico_close02.png" alt="닫기" />
                   </button>
                   <div class="tail" :class="selectedOption"></div>
-                  <p v-if="selectedOption === 'option1'">
-                    귀여운 잠보 먹보 퓨리예요.<br />
-                    움직이기 싫어해서 산책 한번 나가기 힘들지만<br />
-                    막상 나가면 날라다녀요!<br />
-                    6개월째 생일날 받은 노란색 안대는 최애템!
-                  </p>
-                  <p v-else-if="selectedOption === 'option2'">
-                    언제나 씩씩한 빌리의 비밀은<br />
-                    할머니가 준 파란색 담요!<br />
-                    요거 없으면 잠 못자요.
-                  </p>
-                  <p v-else-if="selectedOption === 'option3'">마리 러피 설명</p>
-                  <p v-else-if="selectedOption === 'option4'">도리 러피 설명</p>
+                  <div v-if="selectedOption === 'option1'" class="r_detail01"> 
+                    <p><span>러피 이미지</span><span>Furry Ruffy</span></p>
+                    귀여운 잠보 퓨리예요. 움직이기 싫어해서 산책 한번 나가기 힘들지만 막상 나가면 날라날라~ 6개월째 생일날 받은 노란색 안대는 최애템!
+                  </div>
+                  <div v-else-if="selectedOption === 'option2'" class="r_detail02">
+                    <p><span>러피 이미지</span><span>Billy Ruffy</span></p>
+                    언제나 씩씩한 빌리의 비밀은 할머니가 준 파란색 담요! 이것만 두르면 용기 백배지만 요거 없으면 무서워서 잠도 잘 못자는건 안 비밀~
+                  </div>
+                  <div v-else-if="selectedOption === 'option3'" class="r_detail03">
+                    <p><span>러피 이미지</span><span>Mari Ruffy</span></p>
+                    새침 숙녀 마리예요. 수족냉증이 있어서 사계절 내내 수면양말 네짝은 필수랍니다. 그중 요키 이모가 짜준 핑크 하트 수면 양말이 최고!
+                  </div>
+                  <div v-else-if="selectedOption === 'option4'" class="r_detail04">
+                    <p><span>러피 이미지</span><span>Dory Ruffy</span></p>
+                    세상 순둥순둥하고 착한 도리예요. 두달쯤 되던 달 번개소리에 놀라 잠을 깬 이후로 귀마개가 없으면 잠들지 못하는 짠한 겁보랍니다.
+                  </div>
                 </div>
               </div>
             </div>
