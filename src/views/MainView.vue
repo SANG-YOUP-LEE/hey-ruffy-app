139246714bb4bdf46bd0c88d35a039d33b1b04b9
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-30 11:09:57 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="v_container">
-    <Header @toggle-lnb="isLnbOpen = !isLnbOpen"  />
-    <Lnb v-if="isLnbOpen" @close="isLnbOpen = false" />
-    <div class="main_wrap">
-      <div class="main_inner">
-      </div>
-    </div>
-
-    <Footer />
-    <button class="add_rout" @click="openAddRoutine">
-      <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
-      <span>다짐 추가하기</span>
-    </button>
-  </div>
+  <Header />
+  <Lnb />
+  <button class="add_rout" @click="openAddRoutine">
+    <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
+    <span>다짐 추가하기</span>
+  </button>    
+  <Footer />
 </template>
 
 
