2ebf06bcc12a39e9166a5c71cfb237caf54216bb
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-30 11:10:45 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,11 +1,26 @@
 <template>
   <Header />
   <Lnb />
+
+  <!-- 다짐 추가 버튼 -->
   <button class="add_rout" @click="openAddRoutine">
     <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
     <span>다짐 추가하기</span>
-  </button>    
+  </button>
+
+  <!-- 다짐 추가 팝업 -->
+  <AddRoutineSelector v-if="isAddRoutineOpen" @close="isAddRoutineOpen = false" />
+
   <Footer />
 </template>
 
+<script setup>
+import { ref } from 'vue'
+import AddRoutineSelector from '@/components/AddRoutineSelector.vue'
 
+const isAddRoutineOpen = ref(false)
+
+function openAddRoutine() {
+  isAddRoutineOpen.value = true
+}
+</script>
