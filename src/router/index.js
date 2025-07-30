5ffe56a010d6569eca88f907e58bbaa3cb73053e
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-30 15:50:14 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from "vue-router"
+
 import IntroView from "../views/IntroView.vue"
 import SignupView from "../views/SignupView.vue"
 import LoginView from "../views/LoginView.vue"
 import MainView from "../views/MainView.vue"
+import AddRoutineSelector from "../views/AddRoutineSelector.vue" // 필요 시 라우트로 접근 가능
 
 const routes = [
   { path: "/", name: "intro", component: IntroView },
   { path: "/signup", name: "signup", component: SignupView },
   { path: "/login", name: "login", component: LoginView },
   { path: "/main", name: "main", component: MainView },
-  { path: "/settings", name: "settings", component: SettingsView }, // ✅ 추가됨
+  { path: "/add-routine", name: "addRoutine", component: AddRoutineSelector }, // 옵션
 ]
 
 const router = createRouter({
