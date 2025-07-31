a0b9802e1aaf50a99efab0c8473784a1e141d59d
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-31 15:05:47 +0900

    Update AddRoutineSelector.vue

--- src/views/AddRoutineSelector.vue
+++ src/views/AddRoutineSelector.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="popup_inner">
-    <RoutineTitleInput />
-    <RoutineRepeatSelector />
+  <div class="popup_wrap">
+    <div class="popup_tit">
+      <h2>새로운 다짐을<br />만들어 볼까요?</h2>
+      <p>다짐을 달성할때마다<br />러피의 산책이 총총총 계속됩니다.</p>
+    </div>
+    <div class="popup_inner">
+      <!-- 다짐명 입력-->
+      <RoutineTitleInput />
+      <!-- 다짐 주기 설정 -->
+      <RoutineRepeatSelector />
+    </div>
   </div>
 </template>
 
