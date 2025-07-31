33b68aacd62f004e84492b73ed6c157cab73351f
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-31 14:57:07 +0900

    Update RoutineTitleInput.vue

---
+++ src/components/routine/RoutineTitleInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="form_box_g">
+    <h3>새로운 다짐 제목을적어주세요.</h3>
+    <p>
+      <label for="rt_name">새 다짐 명</label>
+      <input
+        id="rt_name"
+        type="text"
+        placeholder="ex)외로워도 슬퍼도 탄수화물 끊기"
+        class="uline"
+      />
+    </p>
+  </div>
+</template>
