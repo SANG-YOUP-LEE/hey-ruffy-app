b3c9f42c3e7b50f99cd762cff0def9253b7f6175
Ruffy-Cho <oztrickbk@gmail.com>
2025-07-31 16:58:37 +0900

    Update WheelPicker.vue

--- src/components/common/WheelPicker.vue
+++ src/components/common/WheelPicker.vue
@@ -30,7 +30,7 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue'])
 const picker = ref(null)
 let list, items
-const itemHeight = 40
+const itemHeight = 35
 
 const highlight = () => {
   const index = Math.round(list.scrollTop / itemHeight)
