7469cbc00bab7c5d3fe7301fa79620a93b125cde
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-01 10:12:12 +0900

    Update WheelPicker.vue

--- src/components/common/WheelPicker.vue
+++ src/components/common/WheelPicker.vue
@@ -30,12 +30,21 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue'])
 const picker = ref(null)
 let list
+let items
 const itemHeight = 30
 let scrollEndTimer = null
 
-// 스크롤 멈췄을 때 가장 가까운 아이템 선택
+// 스크롤 멈췄을 때 가장 가까운 아이템 선택 및 .light 적용
 const handleScrollEnd = () => {
   const index = Math.round(list.scrollTop / itemHeight)
+
+  if (items && items.length > 0) {
+    items.forEach(item => item.classList.remove('light'))
+    if (items[index]) {
+      items[index].classList.add('light')
+    }
+  }
+
   list.scrollTo({ top: index * itemHeight, behavior: 'smooth' })
   emit('update:modelValue', props.options[index])
 }
@@ -46,22 +55,31 @@ const handleScroll = () => {
   scrollEndTimer = setTimeout(handleScrollEnd, 100)
 }
 
-// 아이템 클릭 시 해당 위치로 스냅
+// 아이템 클릭 시 해당 위치로 스냅 및 .light 적용
 const handleClick = (index) => {
+  if (items && items.length > 0) {
+    items.forEach(item => item.classList.remove('light'))
+    if (items[index]) {
+      items[index].classList.add('light')
+    }
+  }
   list.scrollTo({ top: index * itemHeight, behavior: 'smooth' })
   emit('update:modelValue', props.options[index])
 }
 
 onMounted(() => {
   list = picker.value.querySelector('.scroll-picker-list')
+  items = picker.value.querySelectorAll('.scroll-picker-item')
 
   // 초기값 위치 세팅
   const defaultIndex = props.options.indexOf(props.modelValue)
-  if (defaultIndex !== -1) {
-    nextTick(() => {
-      list.scrollTo({ top: defaultIndex * itemHeight, behavior: 'auto' })
-    })
-  }
+  nextTick(() => {
+    const startIndex = defaultIndex !== -1 ? defaultIndex : 0
+    list.scrollTo({ top: startIndex * itemHeight, behavior: 'auto' })
+    if (items[startIndex]) {
+      items[startIndex].classList.add('light')
+    }
+  })
 
   // 스크롤 이벤트 등록
   list.addEventListener('scroll', handleScroll)
