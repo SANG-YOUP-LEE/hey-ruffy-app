a0dc80c6c99b1a8281be9ab679c0992efabb8d62
SANG-YOUP-LEE <dak7@naver.com>
2025-08-02 08:26:57 +0900

    font외

--- src/views/AddRoutineSelector.vue
+++ src/views/AddRoutineSelector.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="popup_wrap">
     <div class="popup_tit">
-      <h2>새로운 다짐을<br />만들어 볼까요?</h2>
+      <h2>다짐을 만들어 볼까요?</h2>
       <p>다짐을 달성할때마다<br />러피의 산책이 총총총 계속됩니다.</p>
     </div>
     <div class="popup_inner">
