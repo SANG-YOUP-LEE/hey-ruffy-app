1d4181f53caa6a5def3b33118510d1b15ba475f9
SANG-YOUP-LEE <dak7@naver.com>
2025-08-02 17:36:33 +0900

    횟수

--- src/components/routine/RoutineGoalCountSelector.vue
+++ src/components/routine/RoutineGoalCountSelector.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>최소 다짐 달성횟수 선택</div>
+  <div class="form_box_g">
+    <h3>최소 다짐 달성 횟수를 선택해주세요.</h3>
+    <div class="notibox_yellow">
+      선택하신 달성 횟수를 모두 채우면<br />러피의 산책이 1회 완료 됩니다.<br /><strong>완료시엔 산책 완주 보상이 기다리고 있어요!</strong>
+    </div>
+    <div class="goal_select">
+      <div class="custom-radio-group row">
+        <label class="custom-radio">
+          <input type="radio" name="choice" value="goal1" />
+          <span class="circle"></span>
+        </label>
+        <label class="radio-desc" for="goal1">5회</label>
+
+        <label class="custom-radio ">
+          <input type="radio" name="choice" value="goal2" />
+          <span class="circle"></span>
+        </label>
+        <label class="radio-desc" for="goal2">10회</label>
+
+        <label class="custom-radio">
+          <input type="radio" name="choice" value="goal3" />
+          <span class="circle"></span>
+        </label>
+        <label class="radio-desc" for="goal3">15회</label>
+
+         <label class="custom-radio">
+          <input type="radio" name="choice" value="goal4" />
+          <span class="circle"></span>
+        </label>
+        <label class="radio-desc" for="goal4">20회</label>
+      </div>
+    </div>
+  </div>
 </template>
