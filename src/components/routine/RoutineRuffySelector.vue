bfc53af976076b9b0b70969fe74dc352c6125af1
SANG-YOUP-LEE <dak7@naver.com>
2025-08-02 15:54:10 +0900

    러피선택

--- src/components/routine/RoutineRuffySelector.vue
+++ src/components/routine/RoutineRuffySelector.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="form_box_g">
     <h3>함께 산책할 러피를 선택해주세요.</h3>
-    <RuffySelectorSignup v-model="selectedRuffy" />
+    <!-- name을 고유화하기 위해 uniqueName 전달 -->
+    <RuffySelector v-model="selectedRuffy" :unique-name="'routine-choice'" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import RuffySelectorSignup from '@/components/common/RuffySelector.vue'
+import RuffySelector from '@/components/common/RuffySelector.vue'
 
 const selectedRuffy = ref('')
 </script>
