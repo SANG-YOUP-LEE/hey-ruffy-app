dcdce75854059b52a33cc0d603c2f42ed0261a55
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-04 15:43:28 +0900

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="header">
     <div class="left">
-      <a href="#none">lnb</a>
+      <a href="#none" @click.prevent="$emit('toggle-lnb')">lnb</a>
     </div>
     <div class="title">
       <h1>hey, Ruffy!</h1>
       <p>하지 말랑 하지말고 그냥 말랑말랑해~</p>
     </div>
-    <div class="right">
-    </div>
+    <div class="right"></div>
   </div>
 </template>
