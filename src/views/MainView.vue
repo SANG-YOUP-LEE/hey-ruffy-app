79d2aa92e4be66560940435d3779bd5e4a1226fa
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-04 14:42:03 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,10 +5,7 @@
   <!--main view-->
   <div id="main_wrap">
     <!-- 다짐 추가 버튼 -->
-    <button class="add_rout" @click="openAddRoutine">
-      <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
-      <span>다짐 추가하기</span>
-    </button>
+    <button @click="openAddRoutine">다짐 추가하기</button>
 
     <!-- 다짐 추가 팝업 -->
     <AddRoutineSelector v-if="isAddRoutineOpen" @close="isAddRoutineOpen = false" />
