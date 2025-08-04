cc5abd00c6b37dc04c2b662805b5755638a31149
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-04 14:37:26 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,14 +2,20 @@
   <HeaderView />
   <!--LnbView /-->
 
-  <!-- 다짐 추가 버튼 -->
-  <button class="add_rout" @click="openAddRoutine">
-    <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
-    <span>다짐 추가하기</span>
-  </button>
+  <!--main view-->
+  <div id="main_wrap">
+    <!-- 다짐 추가 버튼 -->
+    <button class="add_rout" @click="openAddRoutine">
+      <img src="https://img.icons8.com/?size=100&id=11255&format=png&color=000000">
+      <span>다짐 추가하기</span>
+    </button>
 
-  <!-- 다짐 추가 팝업 -->
-  <AddRoutineSelector v-if="isAddRoutineOpen" @close="isAddRoutineOpen = false" />
+    <!-- 다짐 추가 팝업 -->
+    <AddRoutineSelector v-if="isAddRoutineOpen" @close="isAddRoutineOpen = false" />
+  </div>
+  <!--/main view-->
+
+  
 
   <FooterView />
 </template>
