a96b7c2892603e167b55246a91fb97338b4210bc
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-05 13:36:32 +0900

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -2,9 +2,9 @@
   <div id="footer">
     <div class="button">
       <button class="home"><span>홈</span></button>
+      <button class="shop"><span>상점</span></button>
+      <button class="say"><span>일기</span></button>
       <button class="share"><span>공유</span></button>
-      <button class="like"><span>좋아요</span></button>
-      <button class="setting"><span>설정</span></button>
     </div>
   </div>
 </template>
