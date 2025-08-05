1a171cfa60684770d7a2316b5509d7c23a1c1369
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-05 11:12:11 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -15,7 +15,7 @@
   </div>
 
   <!-- 다짐 추가 버튼 -->
-  <button @click="openAddRoutine" class="ad"><span>다짐 추가하기</span></button>
+  <button @click="openAddRoutine" class="add"><span>다짐 추가하기</span></button>
       
   <!-- 다짐 추가 팝업 -->
   <AddRoutineSelector v-if="isAddRoutineOpen" @close="isAddRoutineOpen = false" />
