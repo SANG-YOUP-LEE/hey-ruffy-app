60ca0c09dcee8aaf2ce88b8167d998831fd388d6
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-06 14:20:32 +0900

    Create MainDateScroll.vue

---
+++ src/components/MainCard/MainDateScroll.vue
@@ -0,0 +1,16 @@
+<template>
+  <!--상단 날짜 휠-->
+  <div class="date_scroll">
+    <div class="date_group">
+      <span
+        v-for="(date, index) in dateList"
+        :key="index"
+        :class="{ on: selectedIndex === index }"
+        @click="selectDate(index)"
+      >
+        <i>{{ getDayLabel(date) }}</i>{{ date.getDate() }}
+      </span>
+    </div>
+  </div>
+  <!--//상단 날짜 휠-->
+</template>
