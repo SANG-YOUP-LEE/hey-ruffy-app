78df8444755ec97e71ed5b72234201f2751c104e
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-06 16:30:47 +0900

    Update MainRoutineTotal.vue

--- src/components/MainCard/MainRoutineTotal.vue
+++ src/components/MainCard/MainRoutineTotal.vue
@@ -3,18 +3,18 @@
   <div class="routine_total">
     <p>
       <span>
-        <strong>오늘의 다짐 현황</strong>
+        <strong>오늘의 다짐</strong>
         총<em>15</em>건 
       </span>
       <span>
-        <strong>미달성</strong> <em>5</em>, 
+        <strong>미달성</strong> <em>5</em>  
         <strong>달성완료</strong> <em>10</em>
       </span>
     </p>
-    <p>
+    <!--p>
       <button>완료한 다짐</button>
       <button>내일의 다짐</button>
       <button>이번주 다짐</button>
-    </p>
+    </p-->
   </div>
 </template>
