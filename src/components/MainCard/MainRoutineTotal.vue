b9c53e1df9a9c1c370bec3ea8ca04a43d69873f0
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-06 14:22:42 +0900

    Create MainRoutineTotal.vue

---
+++ src/components/MainCard/MainRoutineTotal.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--다짐 현황-->
+  <div class="routine_total on_w light">
+    <p>
+      <span><strong>오늘의 다짐</strong> 총<em>15</em>건 중</span>
+      <span><strong>달성전</strong> <em>10</em>건</span>
+      <span><strong>달성완료</strong> <em>5</em>건</span>
+    </p>
+    <p>
+      <button>달성완료 다짐</button>
+      <button>내일의 다짐</button>
+      <button>이번주 다짐</button>
+    </p>
+  </div>
+</template>
