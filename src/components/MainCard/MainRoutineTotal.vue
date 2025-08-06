0784780b16260505943772b68d390dc32f291134
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-06 16:42:21 +0900

    Update MainRoutineTotal.vue

--- src/components/MainCard/MainRoutineTotal.vue
+++ src/components/MainCard/MainRoutineTotal.vue
@@ -5,7 +5,7 @@
       <span>
         <strong>{{ isFuture ? '이날의 다짐' : '오늘의 다짐' }}</strong>
         <template v-if="!isFuture">
-          총<em>15</em>건 
+          총<em class="t_on">15</em>건 
         </template>
       </span>
       <span>
@@ -13,8 +13,8 @@
           미래의 다짐은 편집할 수 없어요.
         </template>
         <template v-else>
-          <strong>미달성</strong> <em>5</em>  
-          <strong>달성완료</strong> <em>10</em>
+          <strong>미달성</strong> <em class="t_on">5</em>  
+          <strong>달성완료</strong> <em class="t_on">10</em>
         </template>
       </span>
     </p>
