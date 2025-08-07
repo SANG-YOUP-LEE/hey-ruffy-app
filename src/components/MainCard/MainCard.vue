9c8e3c62cdd86c143e58fb710b3db0479a0188f0
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-07 09:18:22 +0900

    Update MainCard.vue

--- src/components/MainCard/MainCard.vue
+++ src/components/MainCard/MainCard.vue
@@ -1,22 +1,13 @@
 <template>
   <div class="done_group">
-  <!--달성전-->
-  <div class="not_done">
-    달성전
+    <div v-if="selected === 'notdone'" class="not_done">달성전</div>
+    <div v-if="selected === 'done'" class="done">달성후</div>
+    <div v-if="selected === 'ignored'" class="ignored">흐린눈</div>
   </div>
-  <!--//달성전-->
-
-  <!--달성후-->
-  <div class="done">
-    달성후
-  </div>
-  <!--//달성후-->
-
-  
-  <!--흐린눈-->
-  <div class="ignored">
-    흐린눈
-  </div>
-  <!--//흐린눈-->
 </template>
 
+<script setup>
+defineProps({
+  selected: String
+})
+</script>
