89d20a8013d66be21276082222c728e987c7dda5
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-07 09:13:07 +0900

    Update MainCard.vue

--- src/components/MainCard/MainCard.vue
+++ src/components/MainCard/MainCard.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>MainCard</div>
+  <div class="done_group">
+  <!--달성전-->
+  <div class="not_done">
+    달성전
+  </div>
+  <!--//달성전-->
+
+  <!--달성후-->
+  <div class="done">
+    달성후
+  </div>
+  <!--//달성후-->
+
+  
+  <!--흐린눈-->
+  <div class="ignored">
+    흐린눈
+  </div>
+  <!--//흐린눈-->
 </template>
 
-<script setup>
-</script>
