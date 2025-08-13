632df34a9a2737a20e980c72d2cc96ca422e884d
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-13 13:55:24 +0900

    Create RoutineCardSelector.vue

---
+++ src/components/routine/RoutineCardSelector.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form_box_g">
+    <h3>다짐카드 디자인을 선택해주세요.</h3>
+    <CardSelector v-model="selectedCard" unique-name="routine-card" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CardSelector from '@/components/common/CardSelector.vue'
+
+const selectedCard = ref('')
+
+const setFromRoutine = (routine) => {
+  selectedCard.value = routine?.card || ''
+}
+
+defineExpose({
+  card: selectedCard,
+  setFromRoutine
+})
+
+</script>
