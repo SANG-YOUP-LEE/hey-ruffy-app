42b0f677c466f8fe7584fae728eb7a9a74c9ace5
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-13 16:49:38 +0900

    Update MainWalkStatus.vue

--- src/components/MainCard/MainWalkStatus.vue
+++ src/components/MainCard/MainWalkStatus.vue
@@ -1,17 +1,3 @@
 <template>
-  <div class="walk-status-popup" :class="computedClass">
-    <div class="info">
-      <div class="row"><span class="lbl">러피</span><span class="val">{{ ruffy }}</span></div>
-      <div class="row"><span class="lbl">코스</span><span class="val">{{ course }}</span></div>
-    </div>
-  </div>
+  산책현황
 </template>
-
-<script setup>
-import { computed } from 'vue'
-const props = defineProps({
-  ruffy: { type: String, default: '' },
-  course: { type: String, default: '' }
-})
-const computedClass = computed(() => [props.ruffy, props.course].filter(Boolean).join(' '))
-</script>
