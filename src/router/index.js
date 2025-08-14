5078123599e0d6446bba43633dd48c38c301202d
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-14 18:36:32 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import SignupView from "../views/SignupView.vue"
 import LoginView from "../views/LoginView.vue"
 import MainView from "../views/MainView.vue"
 import AddRoutineSelector from "../views/AddRoutineSelector.vue"
+import { topLoaderStart, topLoaderDone } from "@/lib/topLoader"
 
 const routes = [
   { path: "/", name: "intro", component: IntroView },
@@ -16,5 +17,8 @@ const routes = [
 
 const router = createRouter({ history: createWebHistory(), routes })
 
+router.beforeEach((to, from, next) => { topLoaderStart('route'); next() })
+router.afterEach(() => { topLoaderDone('route') })
+router.onError(() => { topLoaderDone('route') })
 
 export default router
