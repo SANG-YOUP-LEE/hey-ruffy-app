918735b76c9f2effad1375d9fc1ed144c16cfa97
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-14 11:28:26 +0900

    Update MainWalkStatus.vue

--- src/components/MainCard/MainWalkStatus.vue
+++ src/components/MainCard/MainWalkStatus.vue
@@ -1,3 +1,37 @@
+<!-- src/components/MainCard/MainWalkStatus.vue -->
 <template>
-  산책현황
+  <div class="walk_info" v-if="hasWalkResolved">
+    <span class="walk_ruffy">{{ ruffyMeta?.name }}</span>
+    <span class="walk_course">{{ courseMeta?.name }}</span>
+    <span class="walk_goal">목표 {{ routine?.goalCount }}회</span>
+  </div>
+  <div v-else class="walk_info">
+    <span class="walk_empty">산책 정보가 없습니다</span>
+  </div>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import { RUFFY_OPTIONS } from '@/components/common/RuffySelector.vue'
+import { COURSE_OPTIONS } from '@/components/common/CourseSelector.vue'
+
+const props = defineProps({
+  routine: { type: Object, default: () => ({}) }
+})
+
+const routine = computed(() => props.routine || {})
+
+const hasWalkResolved = computed(() => {
+  const r = routine.value
+  if (typeof r.hasWalk === 'boolean') return r.hasWalk
+  return !!r.ruffy && !!r.course && !!r.goalCount
+})
+
+const ruffyMeta = computed(() => {
+  return RUFFY_OPTIONS.find(r => r.value === routine.value?.ruffy) || null
+})
+
+const courseMeta = computed(() => {
+  return COURSE_OPTIONS.find(c => c.value === routine.value?.course) || null
+})
+</script>
