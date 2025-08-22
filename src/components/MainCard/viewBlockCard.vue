85e4cc65ad344d358d57fff1810eccd321e1bdd0
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-22 14:18:17 +0900

    Create viewBlockCard.vue

---
+++ src/components/MainCard/viewBlockCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="['routine_block', cls]">
+    <div class="rb_head">
+      <p class="title">
+        <span :class="ui.colorClass"></span>
+        {{ ui.titleText }}
+      </p>
+      <div class="actions">
+        <button v-if="flags.canShowStatusButton" class="p_white sm" @click="actions.openStatusPopup">현황</button>
+        <button class="p_white sm" @click="expanded = !expanded">{{ expanded ? '접기' : '더 보기' }}</button>
+        <button class="setting sm" @click.stop="actions.togglePopup"><span>설정</span></button>
+      </div>
+    </div>
+
+    <div v-if="showPopup" class="setting_popup">
+      <button class="close_spop" @click="actions.closePopup"><span>설정팝업닫기</span></button>
+      <ul>
+        <li :class="{ disabled: flags.isPaused }">
+          <button class="modify" :disabled="flags.isPaused" @click="actions.onEdit">다짐 수정하기</button>
+        </li>
+        <li>
+          <button class="lock" @click="actions.openPauseRestartConfirm">
+            {{ flags.isPaused ? '다짐 다시 시작하기' : '다짐 잠시 멈추기' }}
+          </button>
+        </li>
+        <li :class="{ disabled: flags.isPaused }">
+          <button class="share" @click="actions.openShareConfirm" :disabled="flags.isPaused">다짐 공유하기</button>
+        </li>
+        <li :class="{ disabled: flags.isPaused }">
+          <button class="del" @click="actions.openDeleteConfirm" :disabled="flags.isPaused">다짐 삭제하기</button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="rb_body" v-if="expanded">
+      <p class="term"><i>{{ ui.repeatLabel }}</i> {{ ui.repeatDetail }}</p>
+      <p class="se_date">{{ ui.periodText }}</p>
+      <p class="alaram">{{ ui.alarmText }}</p>
+      <p class="comment" v-if="ui.commentText">{{ ui.commentText }}</p>
+
+      <div class="walk_info" v-if="flags.hasWalkResolved">
+        <span class="walk_ruffy">{{ ui.ruffyName }}</span>
+        <span class="walk_course">{{ ui.courseName }}</span>
+        <span class="walk_goal">목표 {{ ui.goalCount }}회</span>
+      </div>
+
+      <div class="walk_check" v-if="flags.hasWalkResolved">
+        <button class="p_white" @click="actions.openWalkPopup">산책 현황 보기</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  cls: { type: [Array,Object,String], default: () => [] },
+  ui: { type: Object, required: true },
+  flags: { type: Object, required: true },
+  actions: { type: Object, required: true },
+  showPopup: { type: Boolean, default: false }
+})
+const expanded = ref(false)
+</script>
