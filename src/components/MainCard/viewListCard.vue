95a7c8885fda41c34cf240e3c2363aed2638552f
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-22 15:06:22 +0900

    Update viewListCard.vue

--- src/components/MainCard/viewListCard.vue
+++ src/components/MainCard/viewListCard.vue
@@ -1,12 +1,8 @@
 <template>
-  <div :class="['routine_list', cls]">
-    <div class="rl_head">
-      <p class="title">{{ ui.titleText }}</p>
-      <div class="actions">
-        <button class="p_white sm" @click="expanded = !expanded">{{ expanded ? '접기' : '자세히' }}</button>
-        <button class="setting sm" @click.stop="actions.togglePopup"><span>설정</span></button>
-      </div>
-    </div>
+  <div :class="['vlist', cls]">
+    <button class="setting" @click.stop="actions.togglePopup">
+      <span>다짐설정</span>
+    </button>
 
     <div v-if="showPopup" class="setting_popup">
       <button class="close_spop" @click="actions.closePopup"><span>설정팝업닫기</span></button>
@@ -28,22 +24,38 @@
       </ul>
     </div>
 
-    <div class="rl_body" v-if="expanded">
-      <p class="term"><i>{{ ui.repeatLabel }}</i> {{ ui.repeatDetail }}</p>
-      <p class="se_date">{{ ui.periodText }}</p>
-      <p class="alaram">{{ ui.alarmText }}</p>
-      <p class="comment" v-if="ui.commentText">{{ ui.commentText }}</p>
+    <div class="rc_inner">
+      <div class="left">
+        <p class="title">
+          <span :class="ui.colorClass"></span>
+          {{ ui.titleText }}
+        </p>
+        <p class="term"><i>{{ ui.repeatLabel }}</i> {{ ui.repeatDetail }}</p>
+        <p class="se_date">{{ ui.periodText }}</p>
+        <p class="alaram">{{ ui.alarmText }}</p>
+        <p class="comment" v-if="ui.commentText">{{ ui.commentText }}</p>
+        <div class="walk_info" v-if="flags.hasWalkResolved">
+          <span class="walk_ruffy">{{ ui.ruffyName }}</span>
+          <span class="walk_course">{{ ui.courseName }}</span>
+          <span class="walk_goal">목표 {{ ui.goalCount }}회</span>
+        </div>
+      </div>
 
-      <div class="inline_actions">
-        <button v-if="flags.canShowStatusButton" class="p_white sm" @click="actions.openStatusPopup">현황</button>
-        <button v-if="flags.hasWalkResolved" class="p_white sm" @click="actions.openWalkPopup">산책</button>
+      <div class="right"></div>
+
+      <div :class="['state_button', { twice: flags.hasTwoButtons }]">
+        <div class="done_set" v-if="flags.canShowStatusButton">
+          <button class="p_basic" @click="actions.handleStatusButtonClick">달성현황 체크하기</button>
+        </div>
+        <div class="walk_check" v-if="flags.hasWalkResolved">
+          <button class="p_basic_white" @click="actions.openWalkPopup">산책 현황 보기</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 defineProps({
   cls: { type: [Array,Object,String], default: () => [] },
   ui: { type: Object, required: true },
@@ -51,5 +63,4 @@ defineProps({
   actions: { type: Object, required: true },
   showPopup: { type: Boolean, default: false }
 })
-const expanded = ref(false)
 </script>
