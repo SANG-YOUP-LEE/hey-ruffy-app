3449be95197a69c3ac838d5fdd00a1c033247657
SANG-YOUP-LEE <dak7@naver.com>
2025-08-23 11:26:24 +0900

    ios  tttest

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heyruf.fy',
+  appName: 'HeyRuffy',
+  webDir: 'dist',
+  server: {
+    // 개발 중 iPhone에서 즉시 반영 보고 싶으면 아래 두 줄 켜기
+    // url: 'http://<맥IP>:5173',
+    // cleartext: true
+  }
+};
+
+export default config;
