8f6cc24f9cd6f82164285e263697f087b38be625
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-26 15:39:26 +0900

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="main_wrap" v-cloak :class="{ selecting: deleteMode, 'lnb-open': showLnb }">
     <HeaderView @toggle-lnb="showLnb = !showLnb" :class="{ short: headerShort }" />
-    <transition name="lnb-slide">
-      <LnbView v-if="showLnb" @close-lnb="showLnb = false" />
-    </transition>
+    <LnbView @close-lnb="showLnb = false" />
     <div class="lnb_dim" v-show="showLnb" @click="showLnb = false"></div>
   
     <div id="main_body">
