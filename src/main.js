a69c968186564f2f21141f7fee4ad31c31b94998
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-29 10:06:42 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ import "./assets/css/select.css";
 import "./assets/css/ar_popup.css";
 import "./assets/css/m_routine.css";
 import "./assets/css/walk_status_pop.css";
-import "./assets/js/ui.js";
 
 import VueScrollPicker from "vue-scroll-picker";
 import "vue-scroll-picker/style.css";
