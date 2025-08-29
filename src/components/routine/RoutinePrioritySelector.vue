35317cdadb9b8f3acb59d3cad6892d719e1fc283
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-29 10:43:57 +0900

    Update RoutinePrioritySelector.vue

--- src/components/routine/RoutinePrioritySelector.vue
+++ src/components/routine/RoutinePrioritySelector.vue
@@ -2,19 +2,21 @@
   <div class="form_box_g">
     <h3>다짐을 표현할 색상을 선택해주세요.</h3>
     <div class="color_selecter">
-      <ColorSelector v-model="colorIndex" />
+      <ColorSelector :model-value="modelValue" @update:model-value="updateValue" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { useRoutineFormStore } from '@/stores/routineForm'
 import ColorSelector from '@/components/common/ColorSelector.vue'
 
-const form = useRoutineFormStore()
-const colorIndex = computed({
-  get: () => form.colorIndex,
-  set: v => form.setField('colorIndex', Number.isFinite(+v) ? +v : null)
+const props = defineProps({
+  modelValue: { type: Number, default: null }
 })
+const emit = defineEmits(['update:modelValue'])
+
+function updateValue(v) {
+  const n = Number.isFinite(+v) ? +v : null
+  emit('update:modelValue', n)
+}
 </script>
