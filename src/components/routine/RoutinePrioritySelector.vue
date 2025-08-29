4ee4a8612a91212012fa65bb89c49f0a421a9ce6
Ruffy-Cho <oztrickbk@gmail.com>
2025-08-29 09:22:45 +0900

    Update RoutinePrioritySelector.vue

--- src/components/routine/RoutinePrioritySelector.vue
+++ src/components/routine/RoutinePrioritySelector.vue
@@ -2,30 +2,19 @@
   <div class="form_box_g">
     <h3>다짐을 표현할 색상을 선택해주세요.</h3>
     <div class="color_selecter">
-      <ColorSelector ref="colorSelectorRef" />
+      <ColorSelector v-model="colorIndex" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed } from 'vue'
+import { useRoutineFormStore } from '@/stores/routineForm'
 import ColorSelector from '@/components/common/ColorSelector.vue'
 
-const colorSelectorRef = ref(null)
-
-const setFromRoutine = (routine) => {
-  if (routine?.colorIndex !== undefined && colorSelectorRef.value?.setSelectedColor) {
-    colorSelectorRef.value.setSelectedColor(routine.colorIndex)
-  }
-}
-
-defineExpose({
-  get selectedColor() {
-    return colorSelectorRef.value?.selectedColor ?? null
-  },
-  get priority() {
-    return colorSelectorRef.value?.priority ?? null
-  },
-  setFromRoutine
+const form = useRoutineFormStore()
+const colorIndex = computed({
+  get: () => form.colorIndex,
+  set: v => form.setField('colorIndex', Number.isFinite(+v) ? +v : null)
 })
 </script>
