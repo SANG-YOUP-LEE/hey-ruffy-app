47a6d02156c72235afa1e06ed1e153652a82ac58
Ruffy-Cho <oztrickbk@gmail.com>
2025-09-03 10:52:06 +0900

    Update Footer.vue

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="footer">
     <div class="button">
-      <button class="home" @click="$emit('refresh-main')"><span>홈</span></button>
+      <button class="home" @click="emit('refresh-main')"><span>홈</span></button>
       <button class="shop"><span>상점</span></button>
       <button class="say"><span>일기</span></button>
       <button class="share"><span>공유</span></button>
     </div>
   </div>
 </template>
+
+<script setup>
+const emit = defineEmits(['refresh-main'])
+</script>
+
