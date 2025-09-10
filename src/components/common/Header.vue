b0a2d8f1c988d6e7701c13dbe1be569b9f5aed42
Ruffy-Cho <oztrickbk@gmail.com>
2025-09-10 16:51:33 +0900

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -8,12 +8,11 @@
       </p>
       <div class="title">
         <h1>hey, Ruffy!</h1>
-        <p>하지 말랑 하지말고 그냥 말랑말랑해~</p>
+        <p>하지말랑 하지말고 그냥 말랑말랑해~</p>
       </div>
     </div>
     <div class="right">
       <button class="lnb" @click.prevent="$emit('toggle-lnb')"><span>메뉴열기</span></button>
-      <button class="calendar"><span>달력열기</span></button>
     </div>
   </div>
 </template>
