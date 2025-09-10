4e56a72364746d7ac05c421d279f02b3d6978a0c
Ruffy-Cho <oztrickbk@gmail.com>
2025-09-10 14:44:32 +0900

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -9,7 +9,7 @@
         <p>하지 말랑 하지말고 그냥 말랑말랑해~</p>
       </div>
       <div class="right">
-        <button class="calendar" @click="showTemp = true"><span>달력열기</span></button>
+        <button class="calendar"><span>달력열기</span></button>
       </div>
     </div>
     <div class="ruffys_item">
@@ -18,13 +18,8 @@
       <span class="ruffy03"></span>
       <span class="ruffy04"></span>
     </div>
-    <MainViewTemp v-if="showTemp" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import MainViewTemp from '@/views/MainView_temp.vue'
-
-const showTemp = ref(false)
 </script>
