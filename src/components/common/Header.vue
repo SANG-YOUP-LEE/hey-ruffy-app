588ade412aee9519ec5a823f42c5b17dd7e2da7c
Ruffy-Cho <oztrickbk@gmail.com>
2025-09-10 15:02:24 +0900

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="header">
     <div class="left">
-      <span class="ruffy01"></span>
+      <p class="ruffys"><span></span></p>
       <div class="title">
         <h1>hey, Ruffy!</h1>
         <p>하지 말랑 하지말고 그냥 말랑말랑해~</p>
