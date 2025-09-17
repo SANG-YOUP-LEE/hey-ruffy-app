e529267a064b55dc70a1e1ed9b2873c106d507d9
Ruffy-Cho <oztrickbk@gmail.com>
2025-09-17 15:04:03 +0900

    Update LnbRuffyPick.vue

--- src/views/pages/LnbRuffyPick.vue
+++ src/views/pages/LnbRuffyPick.vue
@@ -1,16 +1,30 @@
 <template>
+  <div class="btn_back">
+    <a href="#none" @click.prevent="handleClose" class="back"><span>이전으로</span></a>
+  </div>
   <div class="page ruffy-pick">
     <header class="page_hd">
-      <button class="back" @click="$router.back()"><span>뒤로</span></button>
-      <h1>캐릭터 변경</h1>
+      <h2>캐릭터 변경</h2>
     </header>
     <main class="page_bd">
       <p>캐릭터를 선택해주세요.</p>
       <div class="ruffy_list">
-        <button @click="pick('option1')">러피 01</button>
-        <button @click="pick('option2')">러피 02</button>
-        <button @click="pick('option3')">러피 03</button>
-        <button @click="pick('option4')">러피 04</button>
+        <button @click="pick('option1')">
+          <img :src="RUFFY_MAP.option1" alt="러피 01" />
+          러피 01
+        </button>
+        <button @click="pick('option2')">
+          <img :src="RUFFY_MAP.option2" alt="러피 02" />
+          러피 02
+        </button>
+        <button @click="pick('option3')">
+          <img :src="RUFFY_MAP.option3" alt="러피 03" />
+          러피 03
+        </button>
+        <button @click="pick('option4')">
+          <img :src="RUFFY_MAP.option4" alt="러피 04" />
+          러피 04
+        </button>
       </div>
     </main>
   </div>
@@ -25,6 +39,13 @@ import { doc, setDoc, serverTimestamp } from 'firebase/firestore'
 const a = useAuthStore()
 const router = useRouter()
 
+const RUFFY_MAP = {
+  option1: new URL('@/assets/images/hey_ruffy_temp01.png', import.meta.url).href,
+  option2: new URL('@/assets/images/hey_ruffy_temp02.png', import.meta.url).href,
+  option3: new URL('@/assets/images/hey_ruffy_temp03.png', import.meta.url).href,
+  option4: new URL('@/assets/images/hey_ruffy_temp04.png', import.meta.url).href,
+}
+
 async function pick(opt){
   const uid = a.user?.uid
   if (uid) {
@@ -34,6 +55,10 @@ async function pick(opt){
     }, { merge: true })
     await a.refreshProfile()
   }
+  router.back()
+}
+
+function handleClose() {
   router.back()
 }
 </script>
