5bbd48d466b50c07c2f6d614f44015f6f0f1cedc
SANG-YOUP-LEE <dak7@naver.com>
2025-09-24 11:05:16 +0900

    메인뷰 테스트 정지 00033

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,283 +1,183 @@
 <template>
-  <div class="page_wrap" v-cloak>
-    <HeaderView @toggle-lnb="showLnb = !showLnb" />
+  <div class="page_wrap">
+    <!-- LNB -->
+    <SlidePanel :show="route.path === '/lnb'" @close="router.replace('/main')">
+      <LnbView @close="router.replace('/main')" />
+    </SlidePanel>
 
-    <div
-      id="main_wrap"
-      :class="{ selecting: rStore.deleteMode, scrolled: scrolledRef }"
-      ref="scrollEl"
-      @scroll="update"
-    >
-      <SlidePanel :show="panelOpen" @close="closePanel">
-        <LnbView @close="closePanel" />
-      </SlidePanel>
-
-      <div class="pre_head" v-show="!scrolledRef" ref="preHeadEl">
-        <MainDateScroll :selectedDate="selectedDate" @selectDate="onSelectDate" />
-        <MainRoutineTotal
-          :key="rtResetKey"
-          :isFuture="isFutureDate"
-          :selectedDate="selectedDate"
-          v-model:modelValue="rStore.selectedFilter"
-          :counts="mv.headerCounts"
-          :totalCount="mv.headerTotal"
-          :periodMode="rStore.selectedPeriod"
-          @requestPrev="onRequestPrev"
-          @requestNext="onRequestNext"
-          @changePeriod="onChangePeriod"
-        />
-      </div>
-
-      <div class="main_scroll">
-        <div class="pre_spacer" :style="{ height: (scrolledRef ? 0 : preHeadH) + 'px' }"></div>
-
-        <div v-show="hasFetched">
-          <div class="sticky_head" v-show="scrolledRef">
-            <div class="routine_total sum">
-              <div class="total_area">
-                <div class="date">
-                  <p class="today">
-                    <button class="prev" @click.prevent="onClickPrev"><span>{{ prevLabel }}</span></button>
-                    <span class="y_m">{{ sumYear }}.{{ sumMonth }}.</span>
-                    <em>{{ sumDay }}</em>
-                    <button class="next" @click.prevent="onClickNext"><span>{{ nextLabel }}</span></button>
-                  </p>
-                </div>
-                <div class="r_state">
-                  <a href="#none" class="not_done" :class="{ on: rStore.selectedFilter==='notdone' }" @click.prevent="rStore.setFilter('notdone')">
-                    <span>체크전 다짐</span><img :src="getFace('01')"><em>{{ (mv.headerCounts && mv.headerCounts.notdone) || 0 }}</em>
-                  </a>
-                  <a href="#none" class="done" :class="{ on: rStore.selectedFilter==='done' }" @click.prevent="rStore.setFilter('done')">
-                    <span>달성완료</span><img :src="getFace('02')"><em>{{ (mv.headerCounts && mv.headerCounts.done) || 0 }}</em>
-                  </a>
-                  <a href="#none" class="fail" :class="{ on: rStore.selectedFilter==='faildone' || rStore.selectedFilter==='fail' }" @click.prevent="rStore.setFilter('faildone')">
-                    <span>달성실패</span><img :src="getFace('03')"><em>{{ (mv.headerCounts && (mv.headerCounts.faildone ?? mv.headerCounts.fail)) || 0 }}</em>
-                  </a>
-                  <a href="#none" class="ignore" :class="{ on: rStore.selectedFilter==='ignored' || rStore.selectedFilter==='ign' }" @click.prevent="rStore.setFilter('ignored')">
-                    <span>흐린눈</span><img :src="getFace('04')"><em>{{ (mv.headerCounts && (mv.headerCounts.ignored ?? mv.headerCounts.ign)) || 0 }}</em>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div class="today_tools">
-              <div class="today">
-                <a href="#none" class="prev" @click.prevent="onClickPrev"><span>{{ prevLabel }}</span></a>
-                <span class="term"></span> <em>{{ periodTitle }}</em>
-                <a href="#none" class="next" @click.prevent="onClickNext"><span>{{ nextLabel }}</span></a>
-              </div>
-              <div class="tools">
-                <a href="#none" class="r_card" :class="{ on: activeTool === 'card' }" @click.prevent="onChangeView('card')"><span>다짐카드보기</span></a>
-                <a href="#none" class="r_list" :class="{ on: activeTool === 'list' }" @click.prevent="onChangeView('list')"><span>다짐목록보기</span></a>
-                <a href="#none" :class="[ localDelete ? 'r_del' : 'r_select', { on: activeTool === 'delete' } ]" @click.prevent="toggleDeleteMode"><span>{{ localDelete ? '삭제하기' : '다짐선택' }}</span></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="today_tools" :class="{ sum: scrolledRef }">
-            <div class="today">
-              <a href="#none" class="prev" @click.prevent="onClickPrev"><span>{{ prevLabel }}</span></a>
-              <span class="term"></span> <em>{{ periodTitle }}</em>
-              <a href="#none" class="next" @click.prevent="onClickNext"><span>{{ nextLabel }}</span></a>
-            </div>
-            <div class="tools">
-              <a href="#none" class="r_card" :class="{ on: activeTool === 'card' }" @click.prevent="onChangeView('card')"><span>다짐카드보기</span></a>
-              <a href="#none" class="r_list" :class="{ on: activeTool === 'list' }" @click.prevent="onChangeView('list')"><span>다짐목록보기</span></a>
-              <a href="#none" :class="[ localDelete ? 'r_del' : 'r_select', { on: activeTool === 'delete' } ]" @click.prevent="toggleDeleteMode"><span>{{ localDelete ? '삭제하기' : '다짐선택' }}</span></a>
-            </div>
-          </div>
-
-          <div v-if="isLoading" class="skeleton-wrap"><div class="skeleton-card"></div><div class="skeleton-card"></div></div>
-          <div v-else-if="hasFetched && !filteredRoutines.length" class="no_data">
-            <span v-if="!rStore.items?.length">아직 지켜야할 다짐이 없어요.<br />오른쪽 하단 +버튼을 눌러 다짐을 추가해 볼까요?</span>
-            <span v-else>해당 조건에 맞는 다짐이 없어요.</span>
-          </div>
-          <template v-else>
-            <MainCard
-              v-for="rt in filteredRoutines"
-              :key="String(rt.id||'').split('-')[0]"
-              :selected="rt?.status || 'notdone'"
-              :routine="rt"
-              :isToday="rStore.selectedPeriod==='T' && mv.startOfDay(rt?.assignedDate?new Date(rt.assignedDate):new Date()).getTime()===mv.startOfDay(new Date()).getTime()"
-              :assigned-date="new Date(rt?.assignedDate || mv.periodStartRaw)"
-              :layout="ViewCardSet"
-              :layout-variant="selectedView==='list'?'list':'basic'"
-              :period-mode="rStore.selectedPeriod"
-              :delete-targets="rStore.deleteTargets"
-              :delete-mode="rStore.deleteMode"
-              @changeStatus="onChangeStatus"
-              @delete="onDelete"
-              @edit="openRoutine"
-              @togglePause="onTogglePause"
-              @toggleSelect="rStore.toggleSelect"
-            />
-          </template>
-        </div>
-      </div>
-
-      <FooterView @refresh-main="refreshBinding" />
-      <button @click="openRoutine()" class="add"><span>다짐 추가하기</span></button>
-      <teleport to="body">
-        <AddRoutineSelector
-          v-if="isAddRoutineOpen"
-          @close="isAddRoutineOpen=false; editingRoutine=null"
-          @save="onSaved"
-          :routineToEdit="editingRoutine"
-        />
-      </teleport>
-    </div>
+    <!-- AddRoutine -->
+    <teleport to="body">
+      <AddRoutineSelector
+        v-if="openAdd"
+        @close="closeAdd"
+        @save="onSaved"
+        :routineToEdit="editingRoutine"
+      />
+    </teleport>
   </div>
 </template>
 
 <script setup>
-import { onMounted, onBeforeUnmount, nextTick, watch, watchEffect, computed, ref } from 'vue'
+import { onMounted, onBeforeUnmount, watch, computed, ref, nextTick } from 'vue'
+import { Capacitor } from '@capacitor/core'
 import { useRoute, useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
 
-import AddRoutineSelector from '@/views/AddRoutineSelector.vue'
-import HeaderView from '@/components/common/Header.vue'
-import LnbView from '@/components/common/Lnb.vue'
-import FooterView from '@/components/common/Footer.vue'
-import MainDateScroll from '@/components/MainCard/MainDateScroll.vue'
-import MainRoutineTotal from '@/components/MainCard/MainRoutineTotal.vue'
-import MainCard from '@/components/MainCard/MainCard.vue'
-import ViewCardSet from '@/components/MainCard/viewCardSet.vue'
 import SlidePanel from '@/components/common/SlidePanel.vue'
+import LnbView from '@/components/common/Lnb.vue'
+import AddRoutineSelector from '@/views/AddRoutineSelector.vue'
 
+import { iosMainView } from '@/utils/iosMainView'
 import { useRoutinesStore } from '@/stores/routines'
 import { useMainViewStore } from '@/stores/mainView'
-import { useMainScroll } from '@/composables/useMainScroll'
 import { useRoutineBinding } from '@/composables/useRoutineBinding'
-import { useBulkDelete } from '@/composables/useBulkDelete'
-import { useMainNavigation } from '@/composables/useMainNavigation'
-import { useVH } from '@/composables/useVH'
-import { useModalStore } from '@/stores/modal'
-import { useAuthStore } from '@/stores/auth'
 
 const route = useRoute()
 const router = useRouter()
 
 const rStore = useRoutinesStore()
 const mv = useMainViewStore()
-const modal = useModalStore()
-const { isLoading, hasFetched, isAddRoutineOpen, showLnb, selectedDate, isFutureDate, selectedView, editingRoutine } = storeToRefs(mv)
+const { selectedDate, editingRoutine } = storeToRefs(mv)
+const { initBinding, refreshBinding } = useRoutineBinding(rStore, mv, () => {})
 
-const panelOpen = computed(() => showLnb.value || route.path === '/lnb')
-function closePanel(){
-  showLnb.value = false
-  if (route.path === '/lnb') router.replace('/main')
-}
+const IS_IOS = Capacitor.getPlatform?.() === 'ios'
+let offNative
 
-const { isScrolled: scrolledRef, headerShort: headerShortRef, updateScrollState, scrollEl } = useMainScroll(rStore, mv)
-const update = () => nextTick(updateScrollState)
-
-const { initBinding, disposeBinding, refreshBinding } = useRoutineBinding(rStore, mv, update)
-const { toggle: handleToggleDeleteMode } = useBulkDelete(rStore, mv)
-const nav = useMainNavigation(rStore, mv, { scrolledRef, headerShortRef, update })
-
-const rtResetKey = ref(0)
-const resetGraph = () => { rtResetKey.value++ }
-
-const filteredRoutines = computed(() => {
-  const f = rStore.selectedFilter || 'notdone'
-  return (mv.displayedRoutines || []).filter(rt => (rt?.status || 'notdone') === f)
-})
-
-const onSelectDate = (date, isFuture, isToday) => { resetGraph(); nav.selectDate(date, isFuture, isToday) }
-const onRequestPrev = () => { resetGraph(); nav.navigate(-1) }
-const onRequestNext = () => { resetGraph(); nav.navigate(1) }
-const onChangePeriod = (mode) => { resetGraph(); nav.changePeriod(mode) }
-
-async function onSaved(rt){
-  isAddRoutineOpen.value = false
+// AddRoutine 열림 여부
+const openAdd = computed(() => route.query.add === '1')
+function closeAdd () {
+  router.replace({ path: '/main', query: {} })
   editingRoutine.value = null
-  rStore.setFilter('notdone')
+}
+async function onSaved () {
+  router.replace({ path: '/main', query: {} })
+  editingRoutine.value = null
   await refreshBinding()
-  update()
+  await iosMainView.update(toRenderModel())
 }
 
-async function onChangeStatus({ id, status }){ rStore.changeStatus({ id: String((typeof id==='string'?id:id?.id)).split('-')[0], status, date: mv.dateKey(selectedDate.value) }); rStore.setFilter(status); update() }
-async function onTogglePause({ id, isPaused }){ rStore.togglePause({ id: String((typeof id==='string'?id:id?.id)).split('-')[0], isPaused }); update() }
-async function onDelete(payload){ rStore.deleteRoutines((Array.isArray(payload)?payload:[payload]).map(v=>String((typeof v==='string'?v:v?.id)).split('-')[0])); update() }
-function openRoutine(rt=null){ window.dispatchEvent(new Event('close-other-popups')); editingRoutine.value = rt; isAddRoutineOpen.value = true }
-
-const sumDate = computed(() => new Date(selectedDate.value))
-const sumYear = computed(() => String(sumDate.value.getFullYear()))
-const sumMonth = computed(() => String(sumDate.value.getMonth() + 1).padStart(2, '0'))
-const sumDay = computed(() => String(sumDate.value.getDate()).padStart(2, '0'))
-
-const activeTool = computed(() => (rStore.deleteMode ? 'delete' : selectedView.value))
-const localDelete = computed(() => rStore.deleteMode)
-function onChangeView(view){
-  if (selectedView.value !== view) nav.changeView(view)
-  if (rStore.deleteMode) handleToggleDeleteMode(false)
-}
-function toggleDeleteMode(){ handleToggleDeleteMode(!rStore.deleteMode) }
-function onClickPrev(){ onRequestPrev() }
-function onClickNext(){ onRequestNext() }
-
-const prevLabel = computed(() => rStore.selectedPeriod === 'W' ? '이전주' : rStore.selectedPeriod === 'M' ? '이전달' : '전날')
-const nextLabel = computed(() => rStore.selectedPeriod === 'W' ? '다음주' : rStore.selectedPeriod === 'M' ? '다음달' : '다음날')
-const periodTitle = computed(() => {
-  const f = rStore.selectedFilter
-  if (f === 'notdone') return '달성 체크 전'
-  if (f === 'done') return '달성완료 다짐'
-  if (f === 'faildone' || f === 'fail') return '달성실패 다짐'
-  if (f === 'ignored' || f === 'ign') return '흐린눈 다짐'
-  if (rStore.selectedPeriod === 'W') return '주간 다짐'
-  if (rStore.selectedPeriod === 'M') return '월간 다짐'
-  return '오늘의 다짐'
-})
-
-const auth = useAuthStore()
-const images = import.meta.glob('@/assets/images/ruffy0{1,2,3,4}_face{01,02,03,04}.png', { eager: true, import: 'default' })
-const ruffyBase = computed(() => {
-  const opt = auth.profile?.selectedRuffy || 'option1'
-  const num = String(opt).replace('option','')
-  return `ruffy0${num}`
-})
-function getFace(n){
-  return images[`/src/assets/images/${ruffyBase.value}_face${n}.png`]
-}
-
-const preHeadEl = ref(null)
-const preHeadH = ref(0)
-const measurePreHead = () => { preHeadH.value = preHeadEl.value ? preHeadEl.value.offsetHeight : 0 }
-
-const { initVH, disposeVH } = useVH()
-onMounted(async () => {
-  showLnb.value = false
-  initVH()
-  await initBinding()
-  nextTick(() => {
-    measurePreHead()
-    window.addEventListener('resize', measurePreHead, { passive: true })
-  })
-  update()
-})
-onBeforeUnmount(() => { disposeVH(); disposeBinding(); window.removeEventListener('resize', measurePreHead) })
-
-watchEffect(() => {
-  mv.setLoading(rStore.isLoading)
-  mv.setFetched(rStore.hasFetched)
-})
-
-watch([selectedDate, () => rStore.selectedPeriod, () => mv.headerCounts, () => mv.headerTotal], () => nextTick(measurePreHead))
-
-watchEffect(async () => {
-  if (mv.showBulkDeleteConfirm) {
-    const ok = await modal.confirm({
-      title: '선택한 다짐을 삭제할까요?',
-      message: '삭제된 다짐은 되돌릴 수 없어요.',
-      okText: '삭제',
-      cancelText: '취소',
-    })
-    mv.showBulkDeleteConfirm = false
-    if (ok) {
-      await onDelete(rStore.deleteTargets || [])
-      await modal.confirm({ title: '완료', message: '선택한 다짐을 삭제했어요.', okText: '확인', cancelText: '' })
-      handleToggleDeleteMode(false)
-    }
+// JS → 네이티브 렌더 모델
+function toRenderModel () {
+  const list = mv.displayedRoutines?.length ? mv.displayedRoutines : (rStore.items || [])
+  return {
+    v: 1,
+    routines: (list || []).map((rt) => ({
+      id: String(rt.id || '').split('-')[0],
+      status: rt.status || 'notdone',
+      isPaused: !!rt.isPaused,
+      ui: {
+        titleText: rt.title || '',
+        repeatLabel: rt.repeatLabel || '',
+        repeatDetail: rt.repeatDetail || rt.alarmText || '',
+        periodText: rt.periodText || '',
+        alarmText: rt.alarmText || '',
+        commentText: rt.commentText || '',
+        colorClass: rt.colorClass || 'yellow',
+        ruffyName: rt.ruffyName || '',
+        courseName: rt.courseName || '',
+        goalCount: rt.goalCount || '',
+      },
+    })),
+    footer: {
+      style: 'solid',
+      safePadding: true,
+      buttons: [
+        { id: 'home',  title: '홈',   icon: 'house', enabled: true },
+        { id: 'walk',  title: '산책', icon: 'paw',   enabled: true },
+        { id: 'feed',  title: '피드', icon: 'list',  enabled: true },
+        { id: 'alarm', title: '알림', icon: 'bell',  enabled: true },
+      ],
+    },
   }
+}
+
+onMounted(async () => {
+  if (!IS_IOS) return
+  await initBinding()
+
+  // ✅ 최초 진입시 데이터가 있을 때까지 기다렸다가 present 실행
+  let stopWatch = null
+  const tryPresent = async () => {
+    await nextTick()
+    await iosMainView.present(toRenderModel())
+  }
+
+  stopWatch = watch(
+    [() => mv.displayedRoutines, () => rStore.items, () => rStore.hasFetched],
+    async ([disp, items, fetched]) => {
+      if ((disp?.length || items?.length) && fetched) {
+        await tryPresent()
+        stopWatch?.()
+      }
+    },
+    { deep: true, immediate: true }
+  )
+
+  // 3초 초과시 강제로 실행 (네트워크 늦을 때 대비)
+  setTimeout(async () => {
+    if (await iosMainView.isPresented() === false) {
+      await tryPresent()
+    }
+  }, 3000)
+
+  // 네이티브 액션 → JS 처리
+  offNative = iosMainView.onAction(async (type, payload) => {
+    if (type === 'navigate') {
+      const route = String(payload?.route || '')
+      if (route === '/lnb') {
+        router.replace('/lnb')
+        return
+      }
+      if (route.startsWith('/main') && route.includes('add=1')) {
+        window.dispatchEvent(new Event('close-other-popups'))
+        editingRoutine.value = null
+        router.replace({ path: '/main', query: { add: '1' } })
+        return
+      }
+      router.replace(route || '/main')
+      return
+    }
+
+    if (type === 'status') {
+      rStore.changeStatus({
+        id: payload?.rid,
+        status: 'done',
+        date: mv.dateKey(selectedDate.value),
+      })
+    } else if (type === 'edit') {
+      const rt = rStore.items?.find(x => String(x.id).startsWith(payload?.rid))
+      window.dispatchEvent(new Event('close-other-popups'))
+      editingRoutine.value = rt || null
+      router.replace({ path: '/main', query: { add: '1' } })
+      return
+    } else if (type === 'delete') {
+      rStore.deleteRoutines([payload?.rid].filter(Boolean))
+    } else if (type === 'pauseRestart') {
+      rStore.togglePause({ id: payload?.rid, isPaused: !!payload?.isPaused })
+    }
+
+    if (type === 'footerTap') {
+      const id = payload?.id
+      if (id === 'home')  router.replace('/main')
+      if (id === 'walk')  router.replace('/walk')
+      if (id === 'feed')  router.replace('/feed')
+      if (id === 'alarm') router.replace('/alarm')
+    }
+
+    await iosMainView.update(toRenderModel())
+  })
+  // store 변화 → 네이티브 갱신
+  // store 변화 → 네이티브 갱신
+  watch(
+    [() => mv.displayedRoutines, () => rStore.items, () => rStore.selectedFilter, selectedDate],
+    async () => { await iosMainView.update(toRenderModel()) },
+    { deep: true }
+  )
+})
+
+onBeforeUnmount(() => {
+  if (!IS_IOS) return
+  try { offNative?.() } catch {}
+  iosMainView.dismiss()
 })
 </script>
